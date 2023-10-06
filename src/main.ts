a91d7b6b3d228146ea82c45b60caccf4b83277d2
ViniciusGomes2006 <viniciusgomes2006.dev@gmail.com>
2023-10-06 17:01:34 -0300

    Some API endpoints are created

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,28 @@
 import http from "http";
 
+const users: any = [];
+
 const server = http.createServer((req,res) => {
-	return res.end("Hello world");
+	const {method, url} = req;
+
+	if (method === "GET" && url === "/users") {
+		return res
+			.setHeader("Content-type", "application/json")
+			.end(`lista de usuários: ${JSON.stringify(users)}`);
+	}
+	
+	if (method === "POST" && url === "/users") {
+
+		users.push({
+			id: 1,
+			name: "Vinicius",
+			last_name: "Gomes"
+		});
+	
+		return res.end("Criação de usuário.");
+	}
+
+	return res.end("Hello World");
 });
 
 server.listen(2707);
